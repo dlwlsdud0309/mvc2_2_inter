7912fe285bb16b5bc038a421dae0a70324d170eb
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-24 00:06:06 +0900

    session의 loginId 오류, 수정 필요

--- src/main/java/nb/controller/dbConnectTest5/NbInsertProController.java
+++ src/main/java/nb/controller/dbConnectTest5/NbInsertProController.java
@@ -1,5 +1,6 @@
 package nb.controller.dbConnectTest5;
 
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -9,28 +10,32 @@ import nb.vo.NoticeBoards;
 
 public class NbInsertProController implements NbController{
 
-	//HttpSession session;
+	HttpServletRequest httpSession;
+	//HttpServlet session;
+	//ServletRequest session;
 	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("NbInsertProController mvc2 신호");
 		
 		String title = request.getParameter("title");
-		String loginId = (String)request.getRequestedSessionId();
+		//String loginId = (String)session.getAttribute("loginId");
 		//System.out.println("InsertPro title : "+title);
 		//System.out.println("InsertPro loginId : "+loginId);
 		String content = request.getParameter("content");
 
 		NoticeBoards nb = new NoticeBoards();
 		nb.setTitle(title);
-		nb.setWriter(loginId);
+		//nb.setWriter(loginId);
 		nb.setContent(content);
 
 		NoticeBoardsDao dao = new NoticeBoardsDao();
 		int resultNum = dao.insert(nb);
 		
 		if(resultNum>0){
-			response.sendRedirect("noticeboards.jsp");
+			//response.sendRedirect("noticeboards.jsp");
+			request.getRequestDispatcher("noticeboardsDetail.jsp").forward(request, response);
 		}
+		
 	}
 }
