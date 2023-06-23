1de65c088e92c9a172b2f2b1d4e87bf3b3fdc47e
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-23 22:23:33 +0900

    NbInsertRegController는 화면전환의 기능만 필요, 재작성 후 .do로 접근 확인

--- src/main/java/nb/controller/dbConnectTest5/NbInsertRegController.java
+++ src/main/java/nb/controller/dbConnectTest5/NbInsertRegController.java
@@ -3,29 +3,14 @@ package nb.controller.dbConnectTest5;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nb.controller.NbController;
-import nb.dao.NoticeBoardsDao;
-import nb.vo.NoticeBoards;
 
 public class NbInsertRegController implements NbController{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("NbInsertRegController mvc2 신호");
+		System.out.println("NbRegController mvc2 신호");
 		
-		String title = request.getParameter("title");
-		//String loginId = (String)session.getAttribute("sessionId");
-		String content = request.getParameter("content");
-
-		NoticeBoards nb = new NoticeBoards();
-		nb.setTitle(title);
-		//nb.setWriter(loginId);
-		nb.setContent(content);
-
-		NoticeBoardsDao dao = new NoticeBoardsDao();
-		int resultNum = dao.insert(nb);
-		
-		if(resultNum>0){
-			response.sendRedirect("noticeboards.jsp");
-		}
+		//화면 전환
+		request.getRequestDispatcher("nbInsertReg.jsp").forward(request, response);
 	}
 }
