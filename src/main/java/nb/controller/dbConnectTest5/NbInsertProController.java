3f9d88b8a6a4045c7756b4003c9e4caf0b7b0ea7
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-25 12:35:43 +0900

    request.getAttribute 사용, null값

--- src/main/java/nb/controller/dbConnectTest5/NbInsertProController.java
+++ src/main/java/nb/controller/dbConnectTest5/NbInsertProController.java
@@ -2,7 +2,6 @@ package nb.controller.dbConnectTest5;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import nb.controller.NbController;
 import nb.dao.NoticeBoardsDao;
 import nb.vo.NoticeBoards;
@@ -20,8 +19,9 @@ public class NbInsertProController implements NbController{
 		
 		String title = request.getParameter("title");
 		//String loginId = (String)httpSession.getAttribute("loginId");
-		HttpSession session = request.getSession();
-		String loginId = (String)session.getAttribute("loginId");
+		//HttpSession session = request.getSession(true);
+		//String loginId = (String)session.getAttribute("writerId");
+		String loginId = (String)request.getAttribute("writerId");
 		System.out.println("InsertPro title : "+title);
 		System.out.println("InsertPro loginId : "+loginId);
 		String content = request.getParameter("content");
